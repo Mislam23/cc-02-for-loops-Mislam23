8e8bf408636b0e3f957c50ce5b34fd4beafc34ba
Mohammad Islam <islam.mohammad.student@gmail.com>
2021-09-29 14:33:23 +0000

    finished 1 to 4

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 //1
 function countToTen(){
-  
+    for (let i = 1; i < 11; i++) {
+        console.log(i)
+    }
 }
 
 //2
-function countFromOne(){
-  
+function countFromOne(integer){
+    for (let i = 1; i <= integer; i++) {
+        console.log(i)
+    }
 }
 
 //3
-function countEveryEven(){
-  
+function countEveryEven(integer){
+  for (let i = 1; i < integer + 1; i++) {
+      if (i % 2 === 0) {
+          console.log(i)
+      }
+  }
 }
 
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(integer){
+   for (let i = 1; i < integer + 1; i++) {
+      if (i % 2 === 1) {
+          console.log(i)
+      }
+  }
 }
 
 //5 
-function countEvens(){
+function countEvens(findEven){
   
 }
 
+
+//countToTen()
+//countFromOne(54)
+//countEveryEven(10)
+//countEveryOdd(10)
+countEvens[1,2,3,4,5,6,7,8,9,12]
